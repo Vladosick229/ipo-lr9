76c8ea4eea5fb4f3d74d6daaa789972ed9bb8ae9
Vlad Sevruk <vladworking5@gmail.com>
2024-11-21 15:01:54 +0300

    Task 3 complete

---
+++ collision/isCollisionRect.py
@@ -0,0 +1,22 @@
+class RectCorrectError():
+   
+ def isCollisionRect():
+    first=[(1, 1),(2, 2)]
+    second=[(3, 17),(13, 1)]
+    x1_first=first[0][0]
+    x2_first=first[1][0]
+    x1_second=second[0][0]
+    x2_second=second[1][0]
+    y1_first=first[0][1]
+    y2_first=first[1][1]
+    y1_second=second[0][1]
+    y2_second=second[1][1]
+    if((y2_first<y1_second)or(y2_second<y1_first)):
+        raise RectCorrectError('1й прямоугольник некоректный')
+    elif((x2_first<x1_second) or(x2_second<x1_first)):
+        print("False")
+    else:
+        print("True")
+
+
+ isCollisionRect()
